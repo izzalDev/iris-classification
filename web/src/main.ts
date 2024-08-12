b14055e789b7c88b9b7dae89bfc914b7bf162bca
izzalDev <92254049+izzalDev@users.noreply.github.com>
2024-08-12 19:56:07 +0700

    fix: error on importing model

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,1 +1,30 @@
 import './style.css'
+import * as ort from 'onnxruntime-web';
+
+ort.env.wasm.wasmPaths = '../node_modules/onnxruntime-web/dist/';
+
+async function main() {
+    try {
+        // create a new session and load the specific model.
+        await ort.InferenceSession.create('./model.onnx')
+
+        // prepare inputs. a tensor need its corresponding TypedArray as data
+        // const dataA = Float32Array.from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+        // const tensorA = new ort.Tensor('float32', dataA, [3, 4]);
+
+        // // prepare feeds. use model input names as keys.
+        // const feeds = { X: tensorA };
+
+        // // feed inputs and run
+        // const results = await session.run(feeds);
+
+        // // read from results
+        // const dataC = results.c.data;
+        // document.write(`data of result tensor 'c': ${dataC}`);
+
+    } catch (e) {
+        document.write(`failed to inference ONNX model: ${e}.`);
+    }
+}
+
+main();
